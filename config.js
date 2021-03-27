658dbd592e72a7874962489225e356b55c79b119
CodeLewisYates <lewis_yates@ymail.com>
2021-03-27 11:55:59 +0000

    config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://lewis:Coffeebeans44@cluster0.tc3co.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRETKEY: 'encrypted-a234-0xjh-kliop-adf-hashed-secret10002'
+}
